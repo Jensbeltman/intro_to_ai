fcc0672f66d34c3455d6a888fa58c91b385963e8
Jensbeltman <jensj16@student.sdu.dk>
2019-11-04 09:55:53 +0100

    Update main.py
    Changed the main loop to run at specific frequency

--- main.py
+++ main.py
@@ -2,55 +2,40 @@ print('hello')
 from time import sleep, time
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, SpeedPercent
 from ev3dev2.sensor import  INPUT_1, INPUT_2
-from ev3dev2.sensor.lego import ColorSensor 
+from ev3dev2.sensor.lego import ColorSensor, TouchSensor
 from linefollower import LineFollower
-
-
 
 rightMotor = LargeMotor(OUTPUT_A)
 leftMotor = LargeMotor(OUTPUT_D)
 rightMotor.command = rightMotor.COMMAND_RUN_DIRECT
 leftMotor.command = leftMotor.COMMAND_RUN_DIRECT
-colorSensorCenter = ColorSensor(INPUT_2)
+colSFollower = ColorSensor(INPUT_2)
+colSCrossDetection = ColorSensor(INPUT_2)
+
+stopButton = TouchSensor(INPUT_1)
 print(type(rightMotor),type(colorSensorCenter))
-
-
-print('its me..')
-
-
-refTime = time()
-currentTime = refTime
-runTime = 20
 
 lineFollower = LineFollower(rightMotor, leftMotor,0.25,0,2)
 
-print(currentTime-refTime)
-while(runTime>(currentTime-refTime)):
-    colorIntens = colorSensorCenter.reflected_light_intensity
+crossDetected = False
+turning = False
+pushing = False
+
+if __name__ == "__main_":
+    while True:
+        now = time.time()            # get the time
+        
+        colorIntens = colorSensorCenter.reflected_light_intensity
+        lineFollower.follow(colorIntens)
+
+        if stopButton.is_pressed(): 
+            break
+
+        elapsed = time.time() - now  # how long was it running?
+        time.sleep(0.1-elapsed)       # sleep accordingly so the full iteration takes 1 second
+        
 
 
-    lineFollower.follow(colorIntens)
-    #inputstring = input()
+        
 
-    # if inputstring != '':
-    #     try:
-    #         speed = int(inputstring)
-    #     except:
-    #         print('input value not an interger')
-    # else:
-    #     print( leftMotor.speed, rightMotor.speed)
-    
-
-    currentTime=time()
-    
-
-rightMotor.duty_cycle_sp = 0
-leftMotor.duty_cycle_sp = 0
-
-
-    
-
-
-    
-
-    
+        
